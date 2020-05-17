9a802a388d0256619e12959ab4dc6790959c93d7
Talchyr <itsdaninsky@gmail.com>
2020-05-16 17:05:34 -0700

    create class for handling notedata

---
+++ Assets/NoteHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+usiCollections;
+usiGeneric;
+using UnityEngine;
+
+public class NoteHandler : MonoBehaviour
+{
+	public class Notes
+	{
+		public	float	time;
+		public	float	lineIndex;
+		public	float	lineLayer;
+		public	int		type; // color, im assuming
+		public	int		cutDirection;
+
+		public Notes (float t, float li, float ll, int c, int dir) {
+			time = t;
+			lineIndex = li;
+			lineLayer = ll;
+			type = c;
+			cutDirection = dir;
+		}
+	}
+}
