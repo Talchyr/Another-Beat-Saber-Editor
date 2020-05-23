1ed64840d5a7849617faa860994441810f305763
Talchyr <itsdaninsky@gmail.com>
2020-05-22 22:44:07 -0700

    get note rotation states working

--- Assets/Notebox.cs
+++ Assets/Notebox.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Notebox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public NoteDir noteDirection;
+	public GameObject arrow;
+	public GameObject dot;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+	public enum NoteDir {
+		Down,
+		DownRight,
+		Right,
+		UpRight,
+		Up,
+		UpLeft,
+		Left,
+		DownLeft,
+		Any
+	}
+
+	void Update() { // throwing this in update for now to see if it works
+		if (noteDirection == NoteDir.Any) {
+			dot.SetActive(true);
+			arrow.SetActive(false);
+			gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+		} 
+		else {
+			dot.SetActive(false);
+			arrow.SetActive(true);
+			switch (noteDirection) {
+				case NoteDir.Down:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+					break;
+				case NoteDir.DownRight:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 45);
+					break;
+				case NoteDir.Right:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 90);
+					break;
+				case NoteDir.UpRight:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 135);
+					break;
+				case NoteDir.Up:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 180);
+					break;
+				case NoteDir.UpLeft:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 225);
+					break;
+				case NoteDir.Left:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 270);
+					break;
+				case NoteDir.DownLeft:
+					gameObject.transform.eulerAngles = new Vector3(0, 0, 315);
+					break;
+			}
+		}
+	}
 }
