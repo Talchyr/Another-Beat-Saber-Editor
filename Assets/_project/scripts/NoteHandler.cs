7b3ce241ab6a678bf41716779cbc37de9dcc8f8b
Talchyr <itsdaninsky@gmail.com>
2020-05-24 01:31:57 -0700

    change note array to note list

--- Assets/_project/scripts/NoteHandler.cs
+++ Assets/_project/scripts/NoteHandler.cs
@@ -25,46 +25,45 @@ public class NoteHandler : MonoBehaviour
 
 	public GameObject notePrefab;
 	public float noteSpeed; // hyperspeed
-	public Notes[] loadedNotes;
-
-	//// spawn notes for testing
-	//void Start() {
-	//	SpawnNotes();
-	//}
+	//public Notes[] loadedNotes;
+	public float onMeasure;
 
 	public void SpawnNotes() {
-		///*	
+		// LIST VERSION //
 		// create fake note data
-		Notes[] sampleNotes = new Notes[10];
-		sampleNotes[0] = new Notes(0, 0, 0, 0, 1);
-		sampleNotes[1] = new Notes(1, 1, 1, 0, 7);
-		sampleNotes[2] = new Notes(3, 2, 0, 0, 3);
-		sampleNotes[3] = new Notes(8, 3, 2, 0, 5);
-		sampleNotes[4] = new Notes(9, 0, 0, 1, 0);
-		sampleNotes[5] = new Notes(10, 1, 1, 1, 4);
-		sampleNotes[6] = new Notes(11, 2, 0, 1, 2);
-		sampleNotes[7] = new Notes(12, 3, 2, 1, 6);
-		sampleNotes[8] = new Notes(13, 1, 0, 0, 8);
-		sampleNotes[9] = new Notes(13, 2, 0, 1, 8);	
-		
+		List<Notes> sampleNotes = new List<Notes>();
+		sampleNotes.Add(new Notes(0, 0, 0, 0, 1));
+		sampleNotes.Add(new Notes(1, 1, 1, 0, 7));
+		sampleNotes.Add(new Notes(3, 2, 0, 0, 3));
+		sampleNotes.Add(new Notes(8, 3, 2, 0, 5));
+		sampleNotes.Add(new Notes(9, 0, 0, 1, 0));
+		sampleNotes.Add(new Notes(10, 1, 1, 1, 4));
+		sampleNotes.Add(new Notes(11, 2, 0, 1, 2));
+		sampleNotes.Add(new Notes(12, 3, 2, 1, 6));
+		sampleNotes.Add(new Notes(13, 1, 0, 0, 8));
+		sampleNotes.Add(new Notes(13, 2, 0, 1, 8));
+
 		// load fake note data
-		Notes[] loadedNotes = sampleNotes;
-		//*/
+		List<Notes> loadedNotes = sampleNotes;
 
-
-		// go through note array
+		// go through note list
 		foreach (Notes note in loadedNotes) {
 			// instantiate a new note at offset
 			GameObject g = Instantiate(notePrefab, new Vector3(
-				-1.5f + note.lineIndex,	// x
+				-1.5f + note.lineIndex, // x
 				0.5f + note.lineLayer,  // y
-				note.time * noteSpeed	// z
+				note.time * noteSpeed   // z
 			), Quaternion.identity);
 
 			g.GetComponent<Notebox>().noteType = note.type; // set L/R color
 			g.GetComponent<Notebox>().noteDirection = (Notebox.NoteDir)note.cutDirection; // set cut direction
 		}
-
 	}
 
+	public void MoveForward() {
+
+	}
+	public void MoveBack() {
+
+	}
 }
